9bc458130a4a6c23084bbb1f663c9e6e87d49d84
lanbinbin <lanbinbin@bilibili.com>
2015-12-15 17:36:48 +0800

    基本瞎写

---
+++ index.js
@@ -0,0 +1,19 @@
+var io = require('socket.io')();
+var onlineUsers = {};
+var onlineCount = 0;
+
+io.on('connection', function(socket, obj){
+    socket.on('disconnect', function(){
+        if (onlineUsers.hasOwnProperty(socket.name)) {
+            var obj = {userId:socket.name, userName:onlineUsers[socket.name]};
+            delete onlineUsers[socket.name];
+            onlineCount--;
+            io.emit('logout', {onlineUsers:onlineUsers, onlineCount:onlineCount, user:obj});
+        };
+    });
+    socket.on('message', function(obj){
+        io.emit('message', obj.content);
+        console.log(obj.userName+'说：'+obj.content);
+    });
+});
+io.listen(3000);
